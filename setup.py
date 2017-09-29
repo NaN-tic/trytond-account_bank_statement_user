786fd907550a9ea01399e907347b4785d64ef42a
Oriol Alfonso Riba <oriolalfonso@calidae.com>
2017-09-29 11:35:22 +0000

    setup.py edited online with Bitbucket

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ import ConfigParser
 
 MODULE = 'account_bank_statement_user'
 PREFIX = 'nantic'
-MODULE2PREFIX = {}
+MODULE2PREFIX = {
+    'account_bank_statement': 'trytonspain',
+}
 
 
 def read(fname):
@@ -51,7 +53,7 @@ setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
     description='',
     long_description=read('README'),
-    author='NaNÂ·tic',
+    author='NaN·tic',
     author_email='info@nan-tic.com',
     url='http://www.nan-tic.com/',
     download_url="https://bitbucket.org/nantic/trytond-%s" % MODULE,
