bc3aef7da77eef0c121730b7d797c185cd163766
C?dric Krier <ced@b2ck.com>
2018-03-01 15:05:16 +0100

    Update to 4.7

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .statement import *
+from . import statement
+
 
 def register():
     Pool.register(
-        StatementLine,
+        statement.StatementLine,
         module='account_bank_statement_user', type_='model')
--- statement.py
+++ statement.py
@@ -2,9 +2,9 @@ from trytond.model import fields
 from trytond.pool import PoolMeta
 
 __all__ = ['StatementLine']
-__metaclass__ = PoolMeta
 
 
 class StatementLine:
+    __metaclass__ = PoolMeta
     __name__ = 'account.bank.statement.line'
     user = fields.Many2One('res.user', 'User')
