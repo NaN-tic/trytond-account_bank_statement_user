4e6cfd4bff2a782fd577df2ed7e704f5b55f4165
?ngel ?lvarez <angel@nan-tic.com>
2018-09-10 10:01:53 +0200

    fix to py3

--- statement.py
+++ statement.py
@@ -4,7 +4,6 @@ from trytond.pool import PoolMeta
 __all__ = ['StatementLine']
 
 
-class StatementLine:
-    __metaclass__ = PoolMeta
+class StatementLine(metaclass=PoolMeta):
     __name__ = 'account.bank.statement.line'
     user = fields.Many2One('res.user', 'User')
